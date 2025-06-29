f529334614addc0c93f60ecac8d46dff1985c519
Colin Wheeler <cocoasamurai@gmail.com>
2025-06-28 23:48:12 -0700

    test for description
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/LoggingKitTests/LoggingKitTests.swift
+++ Tests/LoggingKitTests/LoggingKitTests.swift
@@ -4,3 +4,10 @@ import Testing
 @Test func example() async throws {
 	Log.general.info("Hello, World!")
 }
+
+@Test func testErrorDescription() async throws {
+	struct MyError: Error, CustomStringConvertible {
+		var description: String = "This is a test error"
+	}
+	Log.general.error("MyError: \(MyError().description())")
+}
