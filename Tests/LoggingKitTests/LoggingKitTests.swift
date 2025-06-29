936720c007b61f55349683b3d243b2a71ee0dde8
Colin Wheeler <cocoasamurai@gmail.com>
2025-06-28 23:54:11 -0700

    updating for CustomDebugStringConvertable
    Signed-off-by: Colin Wheeler <cocoasamurai@gmail.com>

--- Tests/LoggingKitTests/LoggingKitTests.swift
+++ Tests/LoggingKitTests/LoggingKitTests.swift
@@ -5,10 +5,15 @@ import Testing
 	Log.general.info("Hello, World!")
 }
 
-struct MyError: Error, CustomStringConvertible {
+struct MyError: Error, CustomStringConvertible, CustomDebugStringConvertible {
 	var description: String = "This is a test error"
+	var debugDescription: String = "This is an even more lengthy description."
 }
 
 @Test func testErrorDescription() async throws {
 	Log.general.error("MyError: \(MyError().description())")
+}
+
+@Test func testDebugDescription() async throws {
+	Log.general.error("MyError: \(MyError().debugDescription())")
 }
